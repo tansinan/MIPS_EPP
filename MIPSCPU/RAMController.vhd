ab558c1209204e85debcb6f56cc7f420dac502ff
Tan Si Nan <tansinan_1995@163.com>
2015-11-24 17:17:26 +0800

    Update RAM control model and its demo, add some ASM test case.

--- MIPSCPU/RAMController.vhd
+++ MIPSCPU/RAMController.vhd
@@ -24,31 +24,54 @@ architecture Behavioral of RAMController_c is
 begin
 	result <= phyDataBus;
 	phyRAMEnable <= '0';
-	process(clock, reset)
-	begin	
-		if rising_edge(clock) then
-			if readControl1.enable = '0' then
-				phyRAMWriteEnable <= '1';
-				phyRAMReadEnable <= '0';
-				phyAddressBus <= readControl1.address;
-				phyDataBus <= (others => 'Z');
-			elsif readControl2.enable = '0' then
-				phyRAMWriteEnable <= '1';
-				phyRAMReadEnable <= '0';
-				phyAddressBus <= readControl2.address;
-				phyDataBus <= (others => 'Z');
-			elsif writeControl.enable = '0' then
-				phyRAMWriteEnable <= '0';
-				phyRAMReadEnable <= '1';
-				phyAddressBus <= writeControl.address;
-				phyDataBus <= writeControl.data;
-			else
-				phyRAMWriteEnable <= '0';
-				phyRAMReadEnable <= '0';
-				phyAddressBus <= (others => '0');
-				phyDataBus <= (others => 'Z');
-			end if;
-		end if;
-	end process;
+	phyRAMWriteEnable <=
+		FUNC_DISABLED when readControl1.enable = '0' else
+		FUNC_DISABLED when readControl2.enable = '0' else
+		FUNC_ENABLED when writeControl.enable = '0' else
+		FUNC_DISABLED;
+
+	phyRAMReadEnable <=
+		FUNC_ENABLED when readControl1.enable = '0' else
+		FUNC_ENABLED when readControl2.enable = '0' else
+		FUNC_DISABLED when writeControl.enable = '0' else
+		FUNC_DISABLED;
+		
+	phyAddressBus <=
+		readControl1.address when readControl1.enable = '0' else
+		readControl2.address when readControl2.enable = '0' else
+		writeControl.address when writeControl.enable = '0' else
+		(others => '0');
+		
+	phyDataBus <=
+		(others => 'Z') when readControl1.enable = '0' else
+		(others => 'Z') when readControl2.enable = '0' else
+		writeControl.data when writeControl.enable = '0' else
+		(others => 'Z');
+--	process(clock, reset)
+--	begin	
+--		if rising_edge(clock) then
+--			if readControl1.enable = '0' then
+--				phyRAMWriteEnable <= '1';
+--				phyRAMReadEnable <= '0';
+--				phyAddressBus <= readControl1.address;
+--				phyDataBus <= (others => 'Z');
+--			elsif readControl2.enable = '0' then
+--				phyRAMWriteEnable <= '1';
+--				phyRAMReadEnable <= '0';
+--				phyAddressBus <= readControl2.address;
+--				phyDataBus <= (others => 'Z');
+--			elsif writeControl.enable = '0' then
+--				phyRAMWriteEnable <= '0';
+--				phyRAMReadEnable <= '1';
+--				phyAddressBus <= writeControl.address;
+--				phyDataBus <= writeControl.data;
+--			else
+--				phyRAMWriteEnable <= '0';
+--				phyRAMReadEnable <= '0';
+--				phyAddressBus <= (others => '0');
+--				phyDataBus <= (others => 'Z');
+--			end if;
+--		end if;
+--	end process;
 end architecture;
 
--- MIPSCPU/RAMDemo.vhd
+++ MIPSCPU/RAMDemo.vhd
@@ -12,7 +12,8 @@ entity RAMDemo is
 		phyRAMWriteEnable : out std_logic;
 		phyRAMReadEnable : out std_logic;
 		phyAddressBus : out std_logic_vector(PHYSICS_RAM_ADDRESS_WIDTH - 1 downto 0);
-		phyDataBus : inout std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0)
+		phyDataBus : inout std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0);
+		testSucceeded_out : out std_logic
 	);
 end RAMDemo;
 
@@ -37,6 +38,10 @@ architecture Behavioral of RAMDemo is
 	signal writeControl : RAMWriteControl_t;
 	signal state : std_logic_vector(3 downto 0);
 	signal result : std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0);
+	signal r1 : std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0);
+	signal r2 : std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0);
+	signal r3 : std_logic_vector(PHYSICS_RAM_DATA_WIDTH - 1 downto 0);
+	signal testSucceeded : std_logic;
 begin
 	ramController_e : RAMController_c port map (
 		clock => clock,
@@ -53,6 +58,7 @@ begin
 	);
 	wrn <= '1';
 	rdn <= '1';
+	testSucceeded_out <= testSucceeded;
 	process(clock, reset)
 	begin
 		if reset = '0' then
@@ -60,6 +66,7 @@ begin
 			readControl1.enable <= '1';
 			readControl2.enable <= '1';
 			writeControl.enable <= '1';
+			testSucceeded <= '1';
 		elsif rising_edge(clock) then
 			if state = "0000" then
 				readControl1.enable <= '1';
@@ -67,6 +74,12 @@ begin
 				writeControl.enable <= '0';
 				writeControl.address <= "00000000000000000000";
 				writeControl.data <= "00010001000100010001000100010001";
+				state <= "1000";
+			elsif state = "1000" then
+				readControl1.enable <= '0';
+				readControl2.enable <= '1';
+				writeControl.enable <= '1';
+				readControl1.address <= "00000000000000000000";
 				state <= "0001";
 			elsif state = "0001" then
 				readControl1.enable <= '1';
@@ -89,13 +102,42 @@ begin
 				writeControl.data <= "01110111011101110111011101110111";
 				state <= "0011";
 			elsif state = "0011" then
-				state <= "0100";
-			elsif state = "0100" then
+				state <= "1110";
+			elsif state = "1110" then
 				readControl1.enable <= '0';
 				readControl2.enable <= '1';
 				writeControl.enable <= '1';
 				readControl1.address <= "00000000000000000001";
-			else
+				state <= "0100";
+			elsif state = "0100" then
+				r1 <= result;
+				readControl1.enable <= '0';
+				readControl2.enable <= '1';
+				writeControl.enable <= '1';
+				readControl1.address <= "00000000000000000000";
+				state <= "0101";
+			elsif state = "0101" then
+				r2 <= result;
+				readControl1.enable <= '0';
+				readControl2.enable <= '1';
+				writeControl.enable <= '1';
+				readControl1.address <= "00000000000000000010";
+				state <= "0110";
+			elsif state = "0110" then
+				r3 <= result;
+				readControl1.enable <= '0';
+				readControl2.enable <= '1';
+				writeControl.enable <= '1';
+				readControl1.address <= "00000000000000000000";
+				state <= "1111";
+			elsif state = "1111" then
+				if r1 = "00110011001100110011001100110011"
+				and r2 = "00010001000100010001000100010001"
+				and r3 = "01110111011101110111011101110111" then
+					testSucceeded <= '1';
+				else
+					testSucceeded <= '0';
+				end if;
 			end if;		
 		end if;
 	end process;
