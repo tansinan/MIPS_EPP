7fe99b2290c65e3a8ddef528e0cb91da3b61d2e6
Crystala <crystalian@126.com>
2015-11-26 01:22:17 +0800

    Added lw & sw @ simulator.cpp

--- Simulator/simulator.cpp
+++ Simulator/simulator.cpp
@@ -319,14 +319,89 @@ void printCommandBin(ofstream &testbench, int commandNum, string command, int ca
 	printTestbench(testbench, commandBin, commandNum, cache);
 }
 
+class memoryModule
+{
+protected:
+	static const int ramSize = 8388608, romSize = 16777216;
+	char ram[ramSize], rom[romSize];
+
+public:
+	int write(unsigned int address, char value)
+	{
+		if (address >= 0x80000000)
+		{
+			cout << "Error: Cannot write into ROM, this command will not be executed.\n";
+			cout << "Continue anyway?(y/n)\n";
+			char c;
+			cin >> c;
+			if (c == 'y')
+				return 0;
+			else
+				return 1;
+		}
+		else
+		{
+			if (address >= ramSize || address < 0)
+			{
+				cout << "Error: Address illegal, this command will not be executed.\n";
+				cout << "Continue anyway?(y/n)\n";
+				char c;
+				cin >> c;
+				if (c == 'y')
+					return 0;
+				else
+					return 1;
+			}
+			ram[address] = value;
+		}
+		return 0;
+	}
+	
+	int read(unsigned int address, char* value)
+	{
+		if (address >= 0x80000000)
+		{
+			address -= 0x80000000;
+			if (address >= romSize)
+			{
+				cout << "Error: Address illegal, this command will not be executed.\n";
+				cout << "Continue anyway?(y/n)\n";
+				char c;
+				cin >> c;
+				if (c == 'y')
+					return 0;
+				else
+					return 1;
+			}
+			*value = rom[address];
+		}
+		else
+		{
+			if (address >= ramSize || address < 0)
+			{
+				cout << "Error: Address illegal, this command will not be executed.\n";
+				cout << "Continue anyway?(y/n)\n";
+				char c;
+				cin >> c;
+				if (c == 'y')
+					return 0;
+				else
+					return 1;
+			}
+			*value = ram[address];
+		}
+		return 0;
+	}
+};
 
 int main(int argc, char* argv[])
 {
+	memoryModule* memory = new memoryModule();	
 	int commandNum = 0;
 	ifstream inputFileStream;
 	if(argc < 2)
 	{
-		cout << "No input file specified.";
+		cout << "No input file specified." << endl;
 		return 0;
 	}
 	ofstream testbench;
@@ -350,7 +425,18 @@ int main(int argc, char* argv[])
 			break;
 		else if (command == "add")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] + cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "addu")
 		{
@@ -363,65 +449,222 @@ int main(int argc, char* argv[])
 			c1 = atoi(s1.c_str());
 			c2 = atoi(s2.c_str());
 			c3 = atoi(s3.c_str());
-			cache[c1] = (unsigned int)cache[c2] + (unsigned int)cache[c3];
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) + ((unsigned int)cache[c3]));
 			cache[0] = 0;
 			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "sub")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] - cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "subu")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) - ((unsigned int)cache[c3]));
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "and")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] & cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "or")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] | cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "xor")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] ^ cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "nor")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = ~(cache[c2] | cache[c3]);
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "slt")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			if (cache[c2] < cache[c3])
+				cache[c1] = 1;
+			else
+				cache[c1] = 0;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "sltu")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			if ((unsigned int)cache[c2] < (unsigned int)cache[c3])
+				cache[c1] = 1;
+			else
+				cache[c1] = 0;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "sll")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,shamt;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			shamt = atoi(s3.c_str());
+			cache[c1] = cache[c2] << shamt;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, shamt);
 		}
 		else if (command == "srl")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,shamt;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			shamt = atoi(s3.c_str());
+			cache[c1] = (unsigned int)cache[c2] >> (unsigned int)shamt;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, shamt);
 		}
 		else if (command == "sra")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,shamt;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			shamt = atoi(s3.c_str());
+			cache[c1] = cache[c2] >> shamt;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, shamt);
 		}
 		else if (command == "sllv")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] << cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "srlv")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = (unsigned int)cache[c2] >> (unsigned int)cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "srav")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,c3;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			s3 = s3.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			c3 = atoi(s3.c_str());
+			cache[c1] = cache[c2] >> cache[c3];
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, c3);
 		}
 		else if (command == "jr")
 		{
@@ -451,7 +694,7 @@ int main(int argc, char* argv[])
 			c1 = atoi(s1.c_str());
 			c2 = atoi(s2.c_str());
 			imm = atoi(s3.c_str());
-			cache[c1] = (unsigned int)cache[c2] + (unsigned int)imm;
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) + ((unsigned int)imm));
 			cache[0] = 0;
 			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
@@ -465,7 +708,7 @@ int main(int argc, char* argv[])
 			c1 = atoi(s1.c_str());
 			c2 = atoi(s2.c_str());
 			imm = atoi(s3.c_str());
-			cache[c1] = (unsigned int)cache[c2] & (unsigned int)imm;
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) & ((unsigned int)imm));
 			cache[0] = 0;
 			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
@@ -479,25 +722,66 @@ int main(int argc, char* argv[])
 			c1 = atoi(s1.c_str());
 			c2 = atoi(s2.c_str());
 			imm = atoi(s3.c_str());
-			cache[c1] = (unsigned int)cache[c2] | (unsigned int)imm;
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) | ((unsigned int)imm));
 			cache[0] = 0;
 			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
 		else if (command == "xori")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,imm;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			imm = atoi(s3.c_str());
+			cache[c1] = (unsigned int)(((unsigned int)cache[c2]) ^ ((unsigned int)imm));
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
 		else if (command == "lui")
 		{
-			
+			string s1,s2;
+			int c1,imm;
+			inputFileStream >> s1 >> s2;
+			s1 = s1.substr(1);
+			c1 = atoi(s1.c_str());
+			imm = atoi(s2.c_str());
+			cache[c1] = ((unsigned int)imm) * 65536;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, imm);
 		}
 		else if (command == "lw")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,imm;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			imm = atoi(s3.c_str());
+			unsigned int address = (unsigned int)(cache[c2] + imm);
+			char word[4];
+			for (unsigned int i = 0; i < 4; i++)
+				memory->read(address + i, &word[i]);
+			cache[c1] = *((int*)word);
 		}
 		else if (command == "sw")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,imm;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			imm = atoi(s3.c_str());
+			unsigned int address = (unsigned int)(cache[c2] + imm);
+			char* word = (char*)&cache[c1];
+			for (unsigned int i = 0; i < 4; i++)
+				memory->write(address + i, word[i]);
 		}
 		else if (command == "beq")
 		{
@@ -509,11 +793,37 @@ int main(int argc, char* argv[])
 		}
 		else if (command == "slti")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,imm;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			imm = atoi(s3.c_str());
+			if (cache[c2] < imm)
+				cache[c1] = 1;
+			else
+				cache[c1] = 0;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
 		else if (command == "sltiu")
 		{
-			
+			string s1,s2,s3;
+			int c1,c2,imm;
+			inputFileStream >> s1 >> s2 >> s3;
+			s1 = s1.substr(1);
+			s2 = s2.substr(1);
+			c1 = atoi(s1.c_str());
+			c2 = atoi(s2.c_str());
+			imm = atoi(s3.c_str());
+			if (cache[c2] < (unsigned int)imm)
+				cache[c1] = 1;
+			else
+				cache[c1] = 0;
+			cache[0] = 0;
+			printCommandBin(testbench, commandNum, command, cache, c1, c2, imm);
 		}
 		else if (command == "j")
 		{
