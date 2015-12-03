cd0094a1b1e50c54fcd1342a271701af69282c7a
Tan Si Nan <tansinan_1995@163.com>
2015-12-03 21:46:56 +0800

    Implement CP0 instruction decode for MFC0 and MTC0 (untested).

--- MIPSCPU/Processor/CP0/CP0PipelinePhaseInstructionDecode.vhd
+++ MIPSCPU/Processor/CP0/CP0PipelinePhaseInstructionDecode.vhd
@@ -1,17 +1,53 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.MIPSCPU.all;
+use work.MIPSCP0.all;
 
 entity CP0PipelinePhaseInstructionDecode is
 	port (
 		reset : in std_logic;
 		clock : in std_logic;
 		instruction : in Instruction_t;
-		primaryRegisterFile : in mips_register_file_port;
-		cp0RegisterFile : in CP0RegisterFileOutput_t;
-		pcValue : in CPUData_t
+		primaryRegisterFileData : in mips_register_file_port;
+		primaryRegisterFileControl : out RegisterFileControl_t;
+		cp0RegisterFileData : in CP0RegisterFileOutput_t;
+		cp0RegisterFileControl : out CP0RegisterFileControl_t
 	);
 end entity;
 
 architecture Behavioral of CP0PipelinePhaseInstructionDecode is
 begin
+	process(instruction, primaryRegisterFileData, cp0RegisterFileData)
+		variable rs, rt: RegisterAddress_t;
+		variable rd : CP0RegisterAddress_t;
+		variable moveAddressPrimaryInt : integer;
+		variable moveAddressCP0Int : integer;
+	begin
+		rs := instruction(MIPS_CPU_INSTRUCTION_RS_HI downto MIPS_CPU_INSTRUCTION_RS_LO);
+		rt := instruction(MIPS_CPU_INSTRUCTION_RT_HI downto MIPS_CPU_INSTRUCTION_RT_LO);
+		rd := instruction(MIPS_CPU_INSTRUCTION_RD_HI downto MIPS_CPU_INSTRUCTION_RD_LO);
+		moveAddressPrimaryInt := to_integer(unsigned(rt));
+		moveAddressCP0Int := to_integer(unsigned(rd));
+		if rs = MIPS_CP0_INSTRUCTION_RS_MT then
+			for i in 0 to MIPS_CP0_REGISTER_COUNT - 1 loop
+				if i = moveAddressCP0Int then
+					cp0RegisterFileControl(i).data <= primaryRegisterFileData(moveAddressPrimaryInt);
+					cp0RegisterFileControl(i).operation <= REGISTER_OPERATION_WRITE;
+				else
+					cp0RegisterFileControl(i).data <= (others => '0');
+					cp0RegisterFileControl(i).operation <= REGISTER_OPERATION_READ;
+				end if;
+			end loop;
+			primaryRegisterFileControl.address <= (others => '0');
+			primaryRegisterFileControl.data <= (others => '0');
+		elsif rs = MIPS_CP0_INSTRUCTION_RS_MF then
+			primaryRegisterFileControl.address <= rt;
+			primaryRegisterFileControl.data <= cp0RegisterFileData(moveAddressCP0Int);
+			for i in 0 to MIPS_CP0_REGISTER_COUNT - 1 loop
+				cp0RegisterFileControl(i).data <= (others => '0');
+				cp0RegisterFileControl(i).operation <= REGISTER_OPERATION_READ;
+			end loop;
+		end if;
+	end process;
 end architecture;
