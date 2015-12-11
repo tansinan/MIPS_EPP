f7a61c01c30e0ea1abbcb9573709bce9b67b5654
Tan Si Nan <tansinan_1995@163.com>
2015-12-11 20:19:04 +0800

    Fix the EPC saving bug.

--- MIPSCPU/Processor/CP0/CP0ExceptionHandler.vhd
+++ MIPSCPU/Processor/CP0/CP0ExceptionHandler.vhd
@@ -21,15 +21,21 @@ architecture Behavioral of CP0ExceptionHandler is
 begin
 	process(clock, reset)
 	begin
-		for i in 0 to MIPS_CP0_REGISTER_COUNT - 1 loop
-			cp0RegisterFileControl(i) <= (
-				operation => REGISTER_OPERATION_READ,
-				data => (others => '0')
-			);
-		end loop;
 		if reset = FUNC_ENABLED then
+			for i in 0 to MIPS_CP0_REGISTER_COUNT - 1 loop
+				cp0RegisterFileControl(i) <= (
+					operation => REGISTER_OPERATION_READ,
+					data => (others => '0')
+				);
+			end loop;
 			pcOverrideControl.operation <= REGISTER_OPERATION_READ;
 		elsif rising_edge(clock) then
+			for i in 0 to MIPS_CP0_REGISTER_COUNT - 1 loop
+				cp0RegisterFileControl(i) <= (
+					operation => REGISTER_OPERATION_READ,
+					data => (others => '0')
+				);
+			end loop;
 			if exceptionTrigger.enabled = FUNC_ENABLED then
 				pcOverrideControl.operation <= REGISTER_OPERATION_WRITE;
 				pcOverrideControl.data <= MIPS_CP0_NONBOOT_EXCEPTION_HANDLER;
