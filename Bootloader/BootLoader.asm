eb6728af4bc3b6aea6e7446b1b90d56907a1bf4e
Tan Si Nan <tansinan_1995@163.com>
2015-12-18 17:39:05 +0800

    Bugfix and refactor for Crystala's bootloader.

--- Bootloader/BootLoader.asm
+++ Bootloader/BootLoader.asm
@@ -1,38 +1,89 @@
-; $1: Temp
-; $2: RAM Starting Address
-; $3: RAM Terminate Address
-; $4: Adder
+/*
+ * This is a bootloader for our MIPS_EPP project
+ * Note : MUST COMPILE WITH NO BRANCH DELAY SLOT OPTION ON!
+ */
 
+#include "regdef.h"
+#include "cp0regdef.h"
+#include "asm.h"
 
-; Read address
-lb $1 COMCache($0)
-addu $2 $0 $1
-lb $1 COMCache($0)
-sll $1 $1 8
-addu $2 $2 $1
-lb $1 COMCache($0)
-sll $1 $1 16
-addu $2 $2 $1
-lb $1 COMCache($0)
-sll $1 $1 24
-addu $2 $2 $1
+LEAF(_start)
+  .set mips32
+  .set reorder
+  .set noat
 
-; Write to RAM and count
-Write_to_RAM:
-	addiu $3 $2 65536
-	addu $4 $0 $0
-	lb $1 COMCache($0)
-	sb $1 0($2)
-	addiu $2 $2 1
-	addu $4 $4 $1
-	
-	bne $2 $3 Write_to_RAM
+  /* $10 stores the address of the UART data register */
+  li $10, 0xb40003f8
 
-; Return total count
-sb $4 COMCache($0)
-srl $4 8
-sb $4 COMCache($0)
-srl $4 8
-sb $4 COMCache($0)
-srl $4 8
-sb $4 COMCache($0)
+  /* Register allocations
+  $1: Temp
+  $2: RAM Starting Address
+  $3: RAM Terminate Address
+  $4: Checksum
+  */
+
+  /* Load the address of RAM to write */
+  li $2, 0
+  jal wait_for_read
+  lbu $1, 0($10)
+  addu $2, $0, $1
+
+  jal wait_for_read
+  lbu $1, 0($10)
+  sll $1, $1, 8
+  addu $2, $2, $1
+
+  jal wait_for_read
+  lbu $1, 0($10)
+  sll $1, $1, 16
+  addu $2, $2, $1
+
+  jal wait_for_read
+  lbu $1, 0($10)
+  sll $1, $1, 24
+  addu $2, $2, $1
+
+  /* Write to RAM, 4K each time */
+  li $4, 0
+  addiu $3, $2, 4096
+  write_to_ram:
+    write_one_byte:
+    	lb $1, 0($10)
+    	sb $1, 0($2)
+    	addiu $2, $2, 1
+    	addu $4, $4, $1
+    bne $2, $3, write_to_ram
+
+  /* Write checksum back to serial port */
+  jal wait_for_read
+  sb $4, 0($10)
+  srl $4, 8
+
+  jal wait_for_read
+  sb $4, 0($10)
+  srl $4, 8
+
+  jal wait_for_read
+  sb $4, 0($10)
+  srl $4, 8
+
+  jal wait_for_read
+  sb $4, 0($10)
+
+  wait_for_write:
+    li $16, 0xb40003fC
+    not_ready_for_write:
+      lw $17, 0($16)
+      andi $17, $17, 0x2000
+    beqz $17, not_ready_for_write
+    jr ra
+
+  wait_for_read:
+    li $16, 0xb40003fC
+    not_ready_for_read:
+      lw $17, 0($16)
+      andi $17, $17, 0x100
+    beqz $17, not_ready_for_read
+    jr ra
+
+END(_start)
